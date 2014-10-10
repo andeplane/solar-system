9ccf5b442b59520a36807b1ddcdfbf2eb127aa29
Anders Hafreager <anderhaf@fys.uio.no>
2014-10-10 13:14:38 +0200

    Fixed a bug with celestialbody constructors

--- celestialbody.cpp
+++ celestialbody.cpp
@@ -9,7 +9,9 @@ CelestialBody::CelestialBody(vec3 pos, vec3 vel, double mass_) {
 }
 
 CelestialBody::CelestialBody(double x, double y, double z, double vx, double vy, double vz, double mass_) {
-    CelestialBody(vec3(x,y,z), vec3(vx, vy, vz), mass_);
+    position = vec3(x,y,z);
+    velocity = vec3(vx,vy,vz);
+    mass = mass_;
 }
 
 void CelestialBody::resetForce() {
