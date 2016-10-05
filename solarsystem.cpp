a35e3aa657c4e1ab0428cc84e6a3ab366778d4be
Anders Hafreager <andershaf@gmail.com>
2016-10-05 13:13:52 +0200

    Added xyz line change.

--- solarsystem.cpp
+++ solarsystem.cpp
@@ -68,7 +68,7 @@ void SolarSystem::writeToFile(string filename)
     }
 
     m_file << numberOfBodies() << endl;
-    m_file << "Comment line that needs to be here" << endl;
+    m_file << "Comment line that needs to be here. Balle." << endl;
     for(CelestialBody &body : m_bodies) {
         m_file << "1 " << body.position.x() << " " << body.position.y() << " " << body.position.z() << "\n";
     }
