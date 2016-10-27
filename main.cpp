36ab7b1dce7d04a478387eaf8ef5fadc70ae4fce
Anders Hafreager <andershaf@gmail.com>
2016-10-27 10:29:52 +0200

    Removed unused things in main, added time measurement.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "solarsystem.h"
 #include "euler.h"
 #include "velocityverlet.h"
+#include <ctime>
 using namespace std;
 
 int main(int numArguments, char **arguments)
@@ -20,18 +21,9 @@ int main(int numArguments, char **arguments)
     // Use with: solarSystem.createCelestialBody( position, velocity, mass );
 
     solarSystem.createCelestialBody( vec3(0,0,0), vec3(0,0,0), 1.0 );
-
-    // We don't need to store the reference, but just call the function without a left hand side
     solarSystem.createCelestialBody( vec3(1, 0, 0), vec3(0, 2*M_PI, 0), 3e-6 );
 
-    // To get a list (a reference, not copy) of all the bodies in the solar system, we use the .bodies() function
-    vector<CelestialBody> &bodies = solarSystem.bodies();
-
-    for(int i = 0; i<bodies.size(); i++) {
-        CelestialBody &body = bodies[i]; // Reference to this body
-        cout << "The position of this object is " << body.position << " with velocity " << body.velocity << endl;
-    }
-
+    clock_t begin = clock();
     VelocityVerlet integrator;
     // Euler integrator;
     for(int timestep=0; timestep<numTimesteps; timestep++) {
@@ -39,7 +31,9 @@ int main(int numArguments, char **arguments)
         solarSystem.writeToFile("positions.xyz");
     }
 
-    cout << "I just created my first solar system that has " << solarSystem.bodies().size() << " objects." << endl;
+    double timeElapsed = (clock() - begin) / double(CLOCKS_PER_SEC);
+    cout << "Simulation finished using " << timeElapsed << " seconds." << endl;
+
     return 0;
 }
 
